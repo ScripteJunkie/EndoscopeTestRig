834b2e980533e9a448ec112ce44d39b558f948aa
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2020-01-20 15:58:01 -0500

    Update endofocus.py

--- endofocus.py
+++ endofocus.py
@@ -1,144 +1,55 @@
-# Currently need to solve "[ WARN:1] terminating async callback" after FPS print
-
-from threading import Thread
-exec('from __future__ import print_function')
-#from __future__ import print_function
-from imutils.video import WebcamVideoStream
-from imutils.video import FPS
-import datetime
-import argparse
-import imutils
+from threading import Thread, Lock
 import cv2
 
-
-class FPS:
-    def __init__(self):
-        # store the start time, end time, and total number of frames
-        # that were examined between the start and end intervals
-        self._start = None
-        self._end = None
-        self._numFrames = 0
-
-    def start(self):
-        # start the timer
-        self._start = datetime.datetime.now()
-        return self
-
-    def stop(self):
-        # stop the timer
-        self._end = datetime.datetime.now()
-
-    def update(self):
-        # increment the total number of frames examined during the
-        # start and end intervals
-        self._numFrames += 1
-
-    def elapsed(self):
-        # return the total number of seconds between the start and
-        # end interval
-        return (self._end - self._start).total_seconds()
-
-    def fps(self):
-        # compute the (approximate) frames per second
-        return self._numFrames / self.elapsed()
-
-class WebcamVideoStream:
-    def __init__(self, src=0):
-        # initialize the video camera stream and read the first frame
-        # from the stream
+class WebcamVideoStream :
+    def __init__(self, src = 1, width = 1280, height = 960) :
         self.stream = cv2.VideoCapture(src)
+        focus = 0
+        self.stream.set(255, focus)
+        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
         (self.grabbed, self.frame) = self.stream.read()
+        self.started = False
+        self.read_lock = Lock()
 
-        # initialize the variable used to indicate if the thread should
-        # be stopped
-        self.stopped = False
-
-    def start(self):
-        # start the thread to read frames from the video stream
-        Thread(target=self.update, args=()).start()
+    def start(self) :
+        if self.started :
+            print("Capture Already In Progress")
+            return None
+        self.started = True
+        self.thread = Thread(target=self.update, args=())
+        self.thread.start()
         return self
 
-    def update(self):
-        # keep looping infinitely until the thread is stopped
-        while True:
-            # if the thread indicator variable is set, stop the thread
-            if self.stopped:
-                return
+    def update(self) :
+        while self.started :
+            (grabbed, frame) = self.stream.read()
+            self.read_lock.acquire()
+            self.grabbed, self.frame = grabbed, frame
+            self.read_lock.release()
 
-            # otherwise, read the next frame from the stream
-            (self.grabbed, self.frame) = self.stream.read()
+    def read(self) :
+        self.read_lock.acquire()
+        frame = self.frame.copy()
+        self.read_lock.release()
+        return frame
 
-    def read(self):
-        # return the frame most recently read
-        return self.frame
+    def stop(self) :
+        self.started = False
+        self.thread.join()
+        if self.thread.is_alive():
+            self.thread.join()
 
-    def stop(self):
-        # indicate that the thread should be stopped
-        self.stopped = True
+    def __exit__(self, exc_type, exc_value, traceback) :
+        self.stream.release()
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-n", "--num-frames", type=int, default=100,
-    help="# of frames to loop over for FPS test")
-ap.add_argument("-d", "--display", type=int, default=-1,
-    help="Whether or not frames should be displayed")
-args = vars(ap.parse_args())
+if __name__ == "__main__" :
+    vs = WebcamVideoStream().start()
+    while True :
+        frame = vs.read()
+        cv2.imshow('Progress', frame)
+        if cv2.waitKey(1) == 27 :
+            break
 
-# grab a pointer to the video stream and initialize the FPS counter
-print("[INFO] sampling frames from webcam...")
-stream = cv2.VideoCapture(1)
-fps = FPS().start()
-
-# loop over some frames
-while fps._numFrames < args["num_frames"]:
-    # grab the frame from the stream and resize it to have a maximum
-    # width of 400 pixels
-    (grabbed, frame) = stream.read()
-    #code to move to next frame
-    frame = imutils.resize(frame, width=400)
-    # check to see if the frame should be displayed to our screen
-    if args["display"] > 0:
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-
-    # update the FPS counter
-    fps.update()
-
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-# do a bit of cleanup
-stream.release()
-cv2.destroyAllWindows()
-
-# created a *threaded* video stream, allow the camera sensor to warmup,
-# and start the FPS counter
-print("[INFO] sampling THREADED frames from webcam...")
-vs = WebcamVideoStream(src=0).start()
-fps = FPS().start()
-
-# loop over some frames...this time using the threaded stream
-while fps._numFrames < args["num_frames"]:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
-    frame = vs.read()
-    frame = imutils.resize(frame, width=400)
-
-    # check to see if the frame should be displayed to our screen
-    if args["display"] > 0:
-        cv2.imshow("Frame", frame)
-        key = cv2.waitKey(1) & 0xFF
-
-    # update the FPS counter
-    fps.update()
-
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+    vs.stop()
+    cv2.destroyAllWindows()
